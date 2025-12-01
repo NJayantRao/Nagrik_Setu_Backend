42be457ddef7f122fc384dfdb1a817d8d13ed196
N Jayant Rao <njayantrao@gmail.com>
2025-12-01 17:37:05 +0530

    added cors configuration

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+
 dotenv.config()
 import { db } from "./db.js"
 import { userRouter } from "./routes/userRouter.js"
@@ -9,6 +11,17 @@ import { staffRouter } from "./routes/staffRouter.js"
 import { complaintRouter } from "./routes/complaintRouter.js"
 
 const app = express()
+
+//cors configuratins
+app.use(cors({
+  origin:["http://localhost:5173","http://127.0.0.1:5173"],
+  credentials:true,
+  methods:["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders:["Content-Type","Authorization"]
+}))
+
+app.options(/.*/,cors());
+
 app.use(express.json())
 const port = process.env.PORT || 3000
 
