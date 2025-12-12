bd0b2ca12d0c5f950effe09d4bb35648b54ed96f
N Jayant Rao <njayantrao@gmail.com>
2025-12-13 00:12:56 +0530

    added site url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 
 //cors configuratins
 app.use(cors({
-  origin:["http://localhost:5173","http://127.0.0.1:5173","https://nagrik-setu.netlify.app"],
+  origin:["http://localhost:5173","http://127.0.0.1:5173","https://nagrik-setu.netlify.app","https://www.nagriksetu.site"],
   credentials:true,
   methods:["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders:["Content-Type","Authorization"]
